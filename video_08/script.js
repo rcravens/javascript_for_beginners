a437442e07bf2dbd3f291d3989a8aa61729e053a
rcravens <bob.cravens@gmail.com>
2025-04-25 11:41:07 -0500

    defining and calling functions

---
+++ video_08/script.js
@@ -0,0 +1,28 @@
+// Script instructions go here!!!
+
+
+
+// Function Declaration
+
+function greet()
+{
+    console.log('Hello from the greet function');
+}
+greet();
+
+// Function Expression
+
+const say_hi = function()
+{
+    console.log('Hi from the say_hi function');
+}
+say_hi();
+
+
+// Arrow Functions
+
+const wave = () => {
+    console.log('Waving from the wave function');
+}
+wave();
+

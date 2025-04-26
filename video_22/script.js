64bdfa058248ff7e07a73a0c468b50283ed727ca
rcravens <bob.cravens@gmail.com>
2025-04-26 18:52:53 -0500

    changes

--- video_22/script.js
+++ video_22/script.js
@@ -7,28 +7,25 @@ function Person(name, age) {
     this.name = name;
     this.age = age;
 
+    // Not what you want to do!!!!!
     this.greet = function () {
-        console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`);
+        console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`)
     }
 }
 
 const bob = new Person('Bob', 45);
-console.log(bob);
-console.log('age', bob.age);
-console.log('name', bob.name);
-bob.greet();
-
 const alice = new Person('Alice', 25);
+bob.greet();
 alice.greet();
 
 console.log(bob.greet === alice.greet);
 
-
 // What is a Prototype?
 // - the base / parent object that our object inherits from
+console.log(bob);
 
 Person.prototype.greet2 = function () {
-    console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`);
+    console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`)
 }
 bob.greet2();
 alice.greet2();
@@ -36,18 +33,17 @@ alice.greet2();
 console.log(bob.greet2 === alice.greet2);
 
 
-// The CORRECT way
+// THE CORRECT WAY
 function Student(name, age) {
     this.name = name;
     this.age = age;
 }
 
 Student.prototype.greet = function () {
-    console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`);
+    console.log(`Hi! My name is ${this.name}. My age is ${this.age} years old.`)
 }
-const joe = new Student('Joe', 18);
-const sara = new Student('Sara', 19);
+
+const joe = new Student('Joe', 19);
+const sara = new Student('Sara', 20);
 joe.greet();
 sara.greet();
-
-console.log(joe.greet === sara.greet);

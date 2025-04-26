dad5565ae0a38ca7488204a7529811eac958ac31
rcravens <bob.cravens@gmail.com>
2025-04-26 13:03:23 -0500

    changes

--- video_10/script.js
+++ video_10/script.js
@@ -2,55 +2,49 @@
 
 
 // Functions Can Be Assigned To Variables
-const say_hi = function()
-{
+const say_hi = function () {
     console.log('Hi from the say_hi function');
 }
-// say_hi();
+say_hi();
 
 // Passing Functions As Arguments
-function whisper(message)
-{
+function whisper(message) {
     console.log(message.toLowerCase());
 }
-// whisper('Hello from Bob');
 
-function shout(message)
-{
+// whisper('ABC');
+
+function shout(message) {
     console.log(message.toUpperCase());
 }
-// shout('Hello from Bob');
 
-function speak(message, func)
-{
+// shout('abc');
+
+function speak(message, func) {
     func(message);
 }
-// speak('Hello from Bob', shout);
 
+speak('Hello', whisper);
+speak('go away', shout);
 
 // Simple Callback Example
 
-function process(a, b, success_callback, failure_callback)
-{
+function process(a, b, success_callback, failure_callback) {
     let sum = a + b;
-
-    if(sum > 10)
-    {
+    if (sum > 10) {
         success_callback(sum);
-    }
-    else
-    {
+    } else {
         failure_callback(sum);
     }
 }
 
-process(5, 3,
-    function(sum)
-                    {
-                        console.log('SUCCESS the sum is', sum);
-                    },
-    function(sum)
-                    {
-                        console.log('FAILURE the sum is', sum);
-                    }
-);
+function success(sum) {
+    console.log('SUCCESS', sum);
+}
+
+function error(sum) {
+    console.log('ERROR', sum);
+}
+
+process(2, 30, success, error);
+process(2, 3, success, error);

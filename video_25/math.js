53b7f858dac532fc985fbd0fd3370d05322edfe5
rcravens <bob.cravens@gmail.com>
2025-04-26 19:43:39 -0500

    modules & imports

---
+++ video_25/math.js
@@ -0,0 +1,27 @@
+// Math Module
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        throw new Error("Cannot divide by zero!");
+    }
+    return x / y;
+}
+
+export default {
+    add,
+    subtract,
+    multiply,
+    divide
+};
---
+++ video_25/script.js
@@ -0,0 +1,9 @@
+// Script instructions go here!!!
+
+// import {add, divide, multiply, subtract} from './math.js'
+import math from './math.js';
+
+console.log('3+2=', math.add(3, 2));
+console.log('9-3=', math.subtract(9, 3));
+console.log('4x5=', math.multiply(4, 5));
+console.log('8/3=', math.divide(8, 3))
